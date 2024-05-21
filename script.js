e86445c0409ff0a0eb4f10e85059a11cc62b2ce8
tomotomoz <tomoko.essvivh@gmail.com>
2024-05-21 17:41:45 +0900

    アップロード

---
+++ script.js
@@ -0,0 +1,7 @@
+const fade = document.querySelector('.fade')
+// const fadeLogo = document.querySelector('.fade-logo')
+document.addEventListener('DOMContentLoaded',function(){
+  setTimeout(() => {
+    fade.classList.add('close')
+  }, 1000);
+})
